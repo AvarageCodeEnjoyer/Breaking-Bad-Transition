6fe29dc797368a43b94b80544e31e4591a9fd402
AvarageCodeEnjoyer <111468739+AvarageCodeEnjoyer@users.noreply.github.com>
2022-09-29 12:47:37 -0700

    Add files via upload

---
+++ JavaScript/OnClick.js
@@ -0,0 +1,12 @@
+var sound = document.getElementById("playSound");
+            
+function imageChange()
+    {
+        document.getElementById('image').src = "/Images/heisenberg.png";
+        
+    }
+
+function playAudio()
+    {
+        sound.play();
+    }
